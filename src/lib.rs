0f850e30b4882007bcdb4a8a987e08866f920ebc
Elliot Bobrow <elliotgreybobrow@gmail.com>
2022-07-02 13:53:13 -0700

    proof of concept

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+// TODO: macro
+pub fn partial<A: Copy, B, C>(f: impl Fn(A, B) -> C, arg1: A) -> impl Fn(B) -> C {
+    move |arg2| f(arg1, arg2)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn partial_works() {
+        let f = |a, b| a + b;
+        assert_eq!(f(2, 3), partial(f, 2)(3));
     }
 }
